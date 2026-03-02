49deb25b3b1334a4aa31c3173a239471ea3f6dda
CatPotat1 <tudor.cristian.murariu.w@gmail.com>
2026-03-02 22:05:37 +0200

    add refresh route

--- backend/routes/login.py
+++ backend/routes/login.py
@@ -1,4 +1,5 @@
 from flask import Blueprint, jsonify, request
+from flask_jwt_extended import jwt_required, get_jwt_identity
 from flask_jwt_extended import (
     create_access_token,
     create_refresh_token
@@ -47,4 +48,16 @@ def login(username, password):
                 "name": user.washer.name
             } if user.washer else None
         }
+    }), 200
+
+
+@login_api.route("/refresh", methods=["POST"])
+@jwt_required(refresh=True)
+def refresh():
+    user_id = get_jwt_identity()
+
+    new_access_token = create_access_token(identity=user_id)
+
+    return jsonify({
+        "access_token": new_access_token
     }), 200
